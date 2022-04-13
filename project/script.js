994904a231921163a8f6b5744d5a7fe40bf709af
Aleksei Naumov <naumov2407@gmail.com>
2022-04-13 16:27:33 -0700

    Не придумал, как улучшить. Предполагаю с помощью деструктуризации.

--- project/script.js
+++ project/script.js
@@ -5,18 +5,19 @@ const goods = [
   { title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = (title, price) => {
+const renderGoodsItem = (title = 'Name', price = 0) => {
   return `
-    <div class="goods-item">
+    <div class="goods-item" style="border: 2px solid; width: 150px; text-align: center; padding-bottom: 10px">
       <h3>${title}</h3>
       <p>${price}</p>
+      <button class="add-button" type="button">Добавить</button>
     </div>
   `;
 };
 
 const renderGoodsList = (list) => {
   let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-  document.querySelector('.goods-list').innerHTML = goodsList;
+  document.querySelector('.goods-list').innerHTML = goodsList.join(" ");
 }
 
 renderGoodsList(goods);
