2a485c16c3c556d773252e7a3913f561df64bd71
SimonVue <viktorov_@mail.ru>
2022-04-15 16:16:35 +0300

    add style.css

--- project/script.js
+++ project/script.js
@@ -5,7 +5,7 @@ const goods = [
   { title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = (title, price) => {
+const renderGoodsItem = (title = 'test', price = 0) => {
   return `
     <div class="goods-item">
       <h3>${title}</h3>
