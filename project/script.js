63b18a63c65206cba5afde184d6e9d485845e0eb
SimonVue <viktorov_@mail.ru>
2022-04-15 17:31:31 +0300

    add good-item style

--- project/script.js
+++ project/script.js
@@ -1,13 +1,14 @@
 const goods = [
-  { title: 'Shirt', price: 150 },
-  { title: 'Socks', price: 50 },
-  { title: 'Jacket', price: 350 },
-  { title: 'Shoes', price: 250 },
+  { image: `img.src='Shoes.png'`, title: 'Shirt', price: 150 },
+  { image: `img.src='Shoes.png'`, title: 'Socks', price: 50 },
+  { image: `img.src='Shoes.png'`, title: 'Jacket', price: 350 },
+  { image: `img.src='Shoes.png'`, title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = (title = 'test', price = 0) => {
+const renderGoodsItem = (image, title = 'test', price) => {
   return `
     <div class="goods-item">
+      <img>${image}</img>
       <h3>${title}</h3>
       <p>${price}</p>
     </div>
