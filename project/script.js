82b13c106c229f5aeb2bfbbcdca695a13e8456ea
n.polupanov <polupanov_90@mail.ru>
2022-05-02 12:56:34 +0300

    upd: lesson_6 - class work

--- project/script.js
+++ project/script.js
@@ -7,15 +7,68 @@ function service(url) {
   .then((res) => res.json())
 }
 
+
+service(GET_BASKET_GOODS_ITEMS).then((data) => {
+  debugger
+})
+
+
 function init() {
+  const CustomButton = Vue.component('custom-button', {
+    template: `
+      <button class="search-button" type="button" v-on:click="$emit('click')">
+         <slot></slot>
+      </button>
+    `
+  })
+  const basketGoods = Vue.component('basket-goods', {
+    data() {
+      return {
+         basketGoodsItems: []
+      }
+    },
+    
+    template: `
+      <div class="fixed-area">
+         <div class="basket-card">
+            <div class="basket-card__header">
+               <h1 class="basket-card__header__title">basket card</h1>
+               <div class="basket-card__header__delete-icon"
+                  v-on:click="$emit('closeclick')"
+               ></div>
+            </div>
+            <div class="basket-card__content">
+               content
+            </div>
+         </div>
+      </div>
+    `
+  })
+  
+  const goodsItem = Vue.component('goods-item', {
+    props: [
+       'item'
+    ],
+    template: `
+      <div class="goods-item">
+         <h3>{{ item.product_name }}</h3>
+         <p>{{ item.price }}</p>
+      </div>
+    `
+  })
+  
   const app = new Vue({
     el: '#root',
     data: {
       items: [],
       filteredItems: [],
-      search: ''
+      search: '',
+      cardIsVision: false
     },
     methods: {
+      setVisionCard() {
+        this.cardIsVision = !this.cardIsVision
+      },
       fetchGoods() {
         service(GET_GOODS_ITEMS).then((data) => {
           this.items = data;
