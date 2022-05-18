efc91817f0b8445f4bd6ff74eaa45ae24e3b140a
Strelok-2010 <karpushin.Vladimir.86@mail.ru>
2022-05-18 15:44:02 +0300

    task1

--- project/script.js
+++ project/script.js
@@ -1,22 +1,26 @@
+"use strict";
+
 const goods = [
-  { title: 'Shirt', price: 150 },
-  { title: 'Socks', price: 50 },
-  { title: 'Jacket', price: 350 },
-  { title: 'Shoes', price: 250 },
+	{ title: 'Shirt', price: 150 },
+	{ title: 'Socks', price: 50 },
+	{ title: 'Jacket', price: 350 },
+	{ title: 'Shoes', price: 250 },
+	{ title: undefined, price: undefined },
 ];
 
-const renderGoodsItem = (title, price) => {
-  return `
+const renderGoodsItem = (title = "Продукт не опознан", price = "Уточните цену") => {
+	return `
     <div class="goods-item">
-      <h3>${title}</h3>
-      <p>${price}</p>
+		<img class="img-tovar" src="img/tovar.jpg" alt="Товар">
+		<h3>Название: ${title}</h3>
+   	<p>Цена: ${price}</p>
     </div>
   `;
 };
 
-const renderGoodsList = (list) => {
-  let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-  document.querySelector('.goods-list').innerHTML = goodsList;
+const renderGoodsList = (list = []) => {
+	let goodsList = list.map(({ title, price }) => renderGoodsItem(title, price)); //деструктуризируем
+	document.querySelector('.goods-list').innerHTML = goodsList.join(''); //склеиваем через пустую строку
 }
 
 renderGoodsList(goods);
