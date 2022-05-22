4f99b43c71276124dc7c2a393216e72a41e4ff8a
Yayooshka <yayooshka@mail.ru>
2022-05-22 12:08:41 +0300

    Solution

--- project/script.js
+++ project/script.js
@@ -5,18 +5,16 @@ const goods = [
   { title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = (title, price) => {
-  return `
-    <div class="goods-item">
+const renderGoodsItem = ({title = '', price = 0}) => 
+  `<div class="goods-item">
       <h3>${title}</h3>
       <p>${price}</p>
-    </div>
-  `;
-};
+    </div>`
+;
 
-const renderGoodsList = (list) => {
-  let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-  document.querySelector('.goods-list').innerHTML = goodsList;
+const renderGoodsList = (list = []) => {
+  let goodsList = list.map(item => renderGoodsItem(item));
+  document.querySelector('.goods-list').innerHTML = goodsList.join('');
 }
 
 renderGoodsList(goods);
