76ea3e12191412d7b374446883ba31a5af8cd3ec
Andromeles <78651329+Andromeles@users.noreply.github.com>
2022-05-27 13:31:16 +0300

    Lesson №1 - Черныш П.А.

--- project/script.js
+++ project/script.js
@@ -5,18 +5,23 @@ const goods = [
   { title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = (title, price) => {
+function renderGoodsItem(title = "Default title", price = "Default price") {
   return `
-    <div class="goods-item">
+    <div class="goods-item">      
       <h3>${title}</h3>
       <p>${price}</p>
+      <button class="add-Price" type="button">Добавить</button>
     </div>
   `;
-};
+}
 
-const renderGoodsList = (list) => {
-  let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-  document.querySelector('.goods-list').innerHTML = goodsList;
+
+/* Поскольку при присваиваеваем возвращенный массив свойству.
+Установка innerHTML переводит массив в строку через запятую; разделитель запятых используется по умолчанию.
+Для того что бы кубрать запятую надо добавить .join('') */
+
+function renderGoodsList(list = {}) {
+  document.querySelector('.goods-list').innerHTML = list.map(item => renderGoodsItem(item.title, item.price)).join('');
 }
 
 renderGoodsList(goods);
